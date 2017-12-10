ae589ad870ddeb98ca7b5e7ae8c315c44e92c22e
Robert Watson <spartan83@gmail.com>
2017-12-10 21:44:35 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,20 +6,20 @@ if name_hash = []
   nil
 end
 index = []
-array = name_hash.collect do |name, value|
+puts name_hash.collect do |name, value|
   index << name
   value
 end
-puts "array #{array}"
-i=0
-lowest= 0
-array_lowest=array[0]
-array.each do |test|
-  if test<array_lowest
-    array_lowest = test
-    lowest = i
-  end
-end
-puts index
-index[lowest]
+# puts "array #{array}"
+# i=0
+# lowest= 0
+# array_lowest=array[0]
+# array.each do |test|
+#   if test<array_lowest
+#     array_lowest = test
+#     lowest = i
+#   end
+# end
+# puts index
+# index[lowest]
 end
